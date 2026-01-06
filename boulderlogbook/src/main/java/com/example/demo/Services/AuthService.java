6d7e09353e6b01139069270632dc27905b6b32b4
Myron Bachynskyy <myronbach07@gmail.com>
2026-01-06 16:17:55 +0100

    fix

--- boulderlogbook/src/main/java/com/example/demo/Services/AuthService.java
+++ boulderlogbook/src/main/java/com/example/demo/Services/AuthService.java
@@ -24,20 +24,19 @@ public class AuthService {
         this.jwtService = jwtService;
     }
 
-    public LoginResponse login(LoginRequest request) {
-        User user = userRepository.findByEmail(request.getEmail())
-            .orElseThrow(() -> new RuntimeException("Invalid credentials"));
+   public LoginResponse login(LoginRequest request) {
+    User user = userRepository.findByEmail(request.getEmail())
+        .orElseThrow(() -> new RuntimeException("Invalid credentials"));
 
-        if (!passwordEncoder.matches(request.getPasswordHash(), user.getPasswordHash())) {
-            throw new RuntimeException("Invalid credentials");
-        }
+    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+        throw new RuntimeException("Invalid credentials");
+    }
 
-            Long userId = user.getId();   
+    Long userId = Long.valueOf(user.getId());
 
     String token = jwtService.generateToken(userId);
     return new LoginResponse(token, userId);
-
-
-        return new LoginResponse(token, user.getId());
-    }
 }
+}
+
+
